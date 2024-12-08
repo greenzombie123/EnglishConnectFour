6244dddd1ef71f12a65435130e04b46c684b4eae
Alvin Hart <105436283+greenzombie123@users.noreply.github.com>
2024-12-09 08:44:25 +0900

    Can create gameboard

--- src/model.ts
+++ src/model.ts
@@ -1,59 +1,123 @@
 export type Player = {
-	playerId:1 | 2,
-	color:Color
-}
+  playerId: 1 | 2;
+  color: Color;
+};
 
-export type CurrentPlayer = Player
+export type CurrentPlayer = Player;
 
 export type Tile = {
-	playerId: 1 | 2 | "empty",
-	color:string,
-	canInsert:boolean,
-	sentence:Sentence,
-}
+  playerId: 1 | 2 | "empty";
+  color: TileColor;
+  canInsert: boolean;
+  sentence: Sentence;
+};
 
-export type XAxisNumber = 1 | 2|3 |4 |5 |6 |7 |8 
+export type XAxisNumber = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8;
 
-export type YAxisNumber = 1 | 2|3 |4 |5 |6 |7 |8 
+export type YAxisNumber = 1 | 2 | 3 | 4 | 5 | 6;
 
-export type XAxisWords = string[]
+export type XAxisWords = [string, string, string, string, string, string, string, string, string, string];
 
-export type YAxisWords = string[]
+export type YAxisWords = [string, string, string, string, string, string]
 
 export type GameBoard = [
-    [Tile,Tile,Tile,Tile,Tile,Tile,Tile,Tile, Tile, Tile],
-    [Tile,Tile,Tile,Tile,Tile,Tile,Tile,Tile, Tile, Tile],
-    [Tile,Tile,Tile,Tile,Tile,Tile,Tile,Tile, Tile, Tile],
-    [Tile,Tile,Tile,Tile,Tile,Tile,Tile,Tile, Tile, Tile],
-    [Tile,Tile,Tile,Tile,Tile,Tile,Tile,Tile, Tile, Tile],
-    [Tile,Tile,Tile,Tile,Tile,Tile,Tile,Tile, Tile, Tile]
-]
+  [Tile, Tile, Tile, Tile, Tile, Tile, Tile, Tile, Tile, Tile],
+  [Tile, Tile, Tile, Tile, Tile, Tile, Tile, Tile, Tile, Tile],
+  [Tile, Tile, Tile, Tile, Tile, Tile, Tile, Tile, Tile, Tile],
+  [Tile, Tile, Tile, Tile, Tile, Tile, Tile, Tile, Tile, Tile],
+  [Tile, Tile, Tile, Tile, Tile, Tile, Tile, Tile, Tile, Tile],
+  [Tile, Tile, Tile, Tile, Tile, Tile, Tile, Tile, Tile, Tile],
+];
 
-export type Color = "red" | "green" | "yellow"| "purple"|"black" | "orange" | "white" | "blue"
+export type Color =
+  | "red"
+  | "green"
+  | "yellow"
+  | "purple"
+  | "black"
+  | "orange"
+  | "white"
+  | "blue";
+export type TileColor = Color | "nocolor";
 
-export type Sentence = string[]
+export type Sentence = string[];
 
-export type GameStatus = "playing" | "setUp" | "gameover"
+export type GameStatus = "playing" | "setUp" | "gameover";
 
-const playerOne:Player = {
-    playerId:1,
-    color:"blue"
-} 
+const xAxisWords: XAxisWords = [
+  "eat",
+  "drink",
+  "sleep",
+  "run",
+  "walk",
+  "study",
+  "use",
+  "watch",
+  "listen",
+  "buy",
+];
+const yAxisWords: YAxisWords = ["I", "He", "She", "You", "They", "We"];
 
-const playerTwo:Player = {
-    playerId:2,
-    color:"red"
-} 
+const playerOne: Player = {
+  playerId: 1,
+  color: "blue",
+};
+
+const playerTwo: Player = {
+  playerId: 2,
+  color: "red",
+};
 
 const players = {
-    1:playerOne,
-    2:playerTwo
-}
+  1: playerOne,
+  2: playerTwo,
+};
 
-let currentPlayer:CurrentPlayer = playerOne
+let currentPlayer: CurrentPlayer = playerOne;
 
-const setCurrentPlayer = (number:1|2)=> currentPlayer = players[number]
-const getCurrentPlayer = ()=> currentPlayer
+const setCurrentPlayer = (number: 1 | 2) => (currentPlayer = players[number]);
+
+const getCurrentPlayer = () => currentPlayer;
+
+const setPlayerColor = (number: 1 | 2, color: Color) =>
+  (players[number] = { ...players[number], color });
+
+const getPlayers = () => players;
+
+const createTile = (firstWord: string, secondWord: string): Tile => ({
+  playerId: "empty",
+  color: "nocolor",
+  canInsert: false,
+  sentence: [firstWord, secondWord],
+});
+
+const createSentence = (firstWord: string, secondWord: string) => [
+  firstWord,
+  secondWord,
+];
+
+const createGameGrid = (xWords:XAxisWords,yWords:YAxisWords):GameBoard => {
+   
+
+    return yWords.map((yword)=>{
+        const row:Tile[] = []
+    
+        xWords.forEach((xword)=>{
+
+            const tile:Tile = createTile(yword, xword)
+            row.push(tile)
+        })
+
+        return row
+    }) as GameBoard
+};
 
 
-export {getCurrentPlayer, setCurrentPlayer}
+export {
+  getCurrentPlayer,
+  setCurrentPlayer,
+  setPlayerColor,
+  getPlayers,
+  createTile,
+  createGameGrid
+};
--- test/model.test.ts
+++ test/model.test.ts
@@ -1,30 +1,101 @@
 import { describe, expect, test } from "@jest/globals";
-import {setCurrentPlayer, getCurrentPlayer, Player} from "../src/model"
+import {
+  setCurrentPlayer,
+  getCurrentPlayer,
+  Player,
+  setPlayerColor,
+  getPlayers,
+  Tile,
+  createTile,
+  GameBoard,
+  createGameGrid,
+  XAxisWords,
+  YAxisWords,
+} from "../src/model";
 
-describe("getCurrentPlayer", ()=>{
+describe("getCurrentPlayer", () => {
+  test("Get current player object", () => {
+    const expected: Player = { playerId: 1, color: "blue" };
+    const result: Player = getCurrentPlayer();
 
-    test("Get current player object", ()=>{
-        const expected:Player = {playerId:1, color:"blue"}
-        const result:Player = getCurrentPlayer()
+    expect(expected).toEqual(result);
+  });
+});
 
-        expect(expected).toEqual(result)
-    })
-})
+describe("setCurrentPlayer", () => {
+  test("Set currentPlayer to player 2", () => {
+    const expected: Player = { playerId: 2, color: "red" };
+    setCurrentPlayer(2);
+    const result: Player = getCurrentPlayer();
 
-describe("setCurrentPlayer", ()=>{
-    
-    test("Set currentPlayer to player 2", ()=>{
-        const expected:Player = {playerId:2, color:"red"}
-        const result:Player = setCurrentPlayer(2)
+    expect(result).toEqual(expected);
+  });
 
-        expect(result).toEqual(expected)
-    })
+  test("Set currentPlayer to player 1", () => {
+    const expected: Player = { playerId: 1, color: "blue" };
+    setCurrentPlayer(2);
+    setCurrentPlayer(1);
+    const result: Player = getCurrentPlayer();
 
-    test("Set currentPlayer to player 1", ()=>{
-        const expected:Player = {playerId:1, color:"blue"}
-        let result:Player = setCurrentPlayer(2)
-        result= setCurrentPlayer(1)
+    expect(result).toEqual(expected);
+  });
+});
 
-        expect(result).toEqual(expected)
-    })
-})
+describe("setPlayerColor", () => {
+  test("Set playerOne color to yellow", () => {
+    const expected: Player = { playerId: 1, color: "yellow" };
+    setPlayerColor(1, "yellow");
+    const result: Player = getPlayers()[1];
+
+    expect(result).toEqual(expected);
+  });
+});
+
+describe("createTile", () => {
+  test("Create a tile object", () => {
+    const expected: Tile = {
+      playerId: "empty",
+      color: "nocolor",
+      sentence: ["I", "study"],
+      canInsert: false,
+    };
+
+    const result: Tile = createTile("I", "study");
+
+    expect(result).toEqual(expected);
+  });
+});
+
+describe("createGameBoard", () => {
+    test("The tile on the 2nd row and last column has a ['He', 'buy'] value in its sentence prop", () => {
+      const expected: Tile = {
+        playerId: "empty",
+        color: "nocolor",
+        sentence: ["He", "buy"],
+        canInsert: false,
+      };
+
+      const xAxisWords: XAxisWords = [
+        "eat",
+        "drink",
+        "sleep",
+        "run",
+        "walk",
+        "study",
+        "use",
+        "watch",
+        "listen",
+        "buy",
+      ];
+      const yAxisWords: YAxisWords = ["I", "He", "She", "You", "They", "We"];
+  
+      const gameBoard: GameBoard = createGameGrid(xAxisWords, yAxisWords);
+
+      const result = gameBoard[1][9]
+
+      console.log(result)
+  
+      expect(result).toEqual(expected);
+    });
+  });
+  
